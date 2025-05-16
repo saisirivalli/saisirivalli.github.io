c954693fa7c761146df88045468a79ff35f1912d
saisirivalli <23r11a0512@gcet.edu.in>
2025-05-16 22:55:09 +0530

    updated

--- a9/script.js
+++ a9/script.js
@@ -1,14 +1,7 @@
-  let fruits=["apple","orange","mango","banana"];
-let result=fruits.filter((value) => orange);
-document.getElementById(res).value=+result;
+  
 const fruits = ["apple", "banana", "mango", "grapes", "orange", "kiwi"];
 
-let result=fruits.filter((value) => apple);
-console.log(result);
-let result=fruits.filter((value) => mango);
-console.log(result);
-let result=fruits.filter((value) => banana);
-console.log(result);
+
 const checkAvailability = () => {
   const input = document.getElementById("fruitInput").value.trim().toLowerCase();
   const result = document.getElementById("result");
